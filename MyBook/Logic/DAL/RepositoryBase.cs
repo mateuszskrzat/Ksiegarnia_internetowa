4c7ba7adeb49060e90c7ec4a50fd89d0dfd8fd11
Mateusz Matejczyk <matejczyk.mateusz@gmail.com>
2020-01-29 13:45:50 +0100

    init proj

---
+++ MyBook/Logic/DAL/RepositoryBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.DAL
+{
+    public class RepositoryBase : DbContext
+    {
+        public RepositoryBase() : base()
+        {
+            this.Connect();
+            Database.Initialize(false);
+        }
+
+        private void Connect() // provide db connection
+        {
+
+        }
+
+        public void Get<T>(Expression<Func<T, bool>> expression)
+        {
+            
+        }
+    }
+}
---
+++ MyBook/Logic/OperationStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class OperationStatus<T>
+    {
+        private bool _status;
+        private Exception _error;
+        private T _data;
+
+        public bool Status { get => _status; set => _status = value; }
+        public Exception Error { get => _error; set => _error = value; }
+        public T Data { get => _data; set => _data = value; }
+
+        public OperationStatus()
+        {
+
+        }
+    }
+}
